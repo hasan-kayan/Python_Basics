9ab26cfc2f2a73c1b2e2e261ac62c605e3848b5b
Hasan Kayan <80827760+hasan-kayan@users.noreply.github.com>
2023-02-03 12:46:26 +0300

    Add files via upload

---
+++ Python Basics/Python-UBN-1.py
@@ -0,0 +1,75 @@
+import numpy as np 
+
+
+
+
+print('Hello World')
+
+# Yorum Satiri 
+# var a int = 2 
+
+a = 2 # int 
+print(a) 
+
+print(type(a))
+
+b= 2.2 #float 
+print(type(b))
+
+c = "UBN-Jr Python Class" #string 
+print(type(c))
+print(c) 
+
+d = True #boolean 
+print(type(d)) 
+# 1-0 ifadesidir 
+print(d) 
+
+e = False #boolean
+print(type(e))
+print(e)
+
+
+ubn = int = 45 
+print(ubn) 
+
+print(c, "Merhaba UBN - Jr ", a, b, d, e, ubn)
+
+deger1 = 2
+deger2 = 3
+print(deger1 + deger2) 
+
+a = input("Lutfen bir sayi giriniz: ") 
+
+print(a) 
+
+
+
+cumle = str
+cumle = input("Lutfen bir cumle giriniz: ") 
+
+print(cumle) 
+
+
+## PYPI - PIP - PYTHON PACKAGE INDEX
+
+
+# Python da ortalma alma kodu 
+
+vize = input("Lutfen vize notunuzu giriniz: ")
+final = input("Lutfen final notunuzu giriniz: ")
+
+
+
+finalortalama = float(final) * 0.6
+vizeortalama = float(vize) * 0.4
+
+
+ortalama = finalortalama + vizeortalama 
+print(ortalama) 
+
+
+# Sartli ifadeler ve donguler 
+
+
+
